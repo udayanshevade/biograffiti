6008f131f5523052d282e76eb8f6ab3cf11da8ca
Udayan Shevade <udayan.shevade@gmail.com>
2016-03-26 07:12:09 -0700

    Split query string via regex

--- src/js/Controllers/MainCtrl.js
+++ src/js/Controllers/MainCtrl.js
@@ -13,10 +13,10 @@ tributeApp.controller('mainCtrl', ['$scope', 'smoothScrollService', '$window', f
 
   $scope.pageId = '';
 
-  $scope.errorMsg = '<h3 class="error">could not find the right page at this time. please try again, or use a different search: alternate spellings, spacing or punctuation can help, e.g. "louis c k" instead of "louis ck."</h3>';
+  $scope.errorMsg = '<h3 class="error">could not find the right page at this time. please try again, or use a different search: correct spelling, spacing or punctuation can help, e.g. "louis c.k." instead of "louis ck".</h3>';
 
   $scope.capitalize = function(str) {
-    var splitStr = str.split(' '),
+    var splitStr = str.split(/[\s,.']+/),
         newStr = [];
     splitStr.forEach(function(substr) {
       newStr.push(
